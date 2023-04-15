1f0db417f63c7f7028e666ccce09afbc8a53326f
Lance Patrick Bassett <lanbas@gl-login1.arc-ts.umich.edu>
2023-04-14 20:54:52 -0400

    slurm training and dataset creation script

---
+++ datasets/combined_A_and_B_better.py
@@ -0,0 +1,33 @@
+import cv2 
+import os 
+import glob
+import argparse
+import pdb
+import numpy as np
+
+def sort_by_index(fname):
+    return int(fname.split("_")[1])
+
+def main(args):
+    # Assumes dir A and dir B contain same amount and same size images
+    img_A_fnames = sorted(os.listdir(args.img_A_dir), key=sort_by_index)
+    img_B_fnames = sorted(os.listdir(args.img_B_dir), key=sort_by_index)
+
+    if not os.path.exists(args.img_AB_dir):
+        os.makedirs(args.img_AB_dir)
+    
+    for i, (a_f, b_f) in enumerate(zip(img_A_fnames, img_B_fnames)):
+        img_a = cv2.imread(os.path.join(args.img_A_dir, a_f), cv2.IMREAD_COLOR)
+        img_b = cv2.imread(os.path.join(args.img_B_dir, b_f), cv2.IMREAD_COLOR)
+        img_ab = np.concatenate([img_a, img_b], 1)
+
+        cv2.imwrite(os.path.join(args.img_AB_dir, str(i) + ".jpg"), img_ab)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--img_A_dir", type=str)
+    parser.add_argument("--img_B_dir", type=str)
+    parser.add_argument("--img_AB_dir", type=str, help="where to save concatenated results")
+
+    args = parser.parse_args()
+    main(args)
---
+++ slurm_training.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --account=eecs598s010w23_class
+#SBATCH --partition=spgpu
+#SBATCH --time=00-01:20:00
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=4
+#SBATCH --mem-per-gpu=32GB
+
+# set up job
+module load python/3.9.12 cuda/11.6.2
+# module load python/3.9.12 cuda/10.2.89
+source env/bin/activate
+# module load python/3.9.12 cuda/10.2.89
+module load python/3.9.12 cuda/11.6.2
+# pushd /home/kpyu/great-lakes-tutorial
+
+
+# run job
+python train.py --dataroot ./datasets/maze_5x5 --name maze5x5_1000_pix2pix_global_D --model pix2pix --direction BtoA 
+
