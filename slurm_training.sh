24e49f5dde1c97a686a282beeab8d936b9554933
JiahangLiGary <jiahang@umich.edu>
2023-04-17 11:47:37 -0400

    changes to discriminators, increased patch size

--- slurm_training.sh
+++ slurm_training.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --account=eecs598s010w23_class
-#SBATCH --partition=spgpu
-#SBATCH --time=00-01:20:00
-#SBATCH --gpus=1
-#SBATCH --cpus-per-gpu=4
-#SBATCH --mem-per-gpu=32GB
+# SBATCH --account=eecs598s010w23_class
+# SBATCH --partition=spgpu
+# SBATCH --time=00-01:20:00
+# SBATCH --gpus=1
+# SBATCH --cpus-per-gpu=4
+# SBATCH --mem-per-gpu=32GB
 
 # set up job
 module load python/3.9.12 cuda/11.6.2
